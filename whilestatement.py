f35d31d93bbf8527bf3af08ac82d6a0f2cab0e47
User <kimseeh957@gmail.com>
2023-08-26 11:40:07 +0900

    while문, break문 기초학습

--- whilestatement.py
+++ whilestatement.py
@@ -1,4 +1,4 @@
-nuber = 0
+number = 0
 
 while number < 10:
     print(number)
@@ -8,18 +8,35 @@ while number < 10:
 # print("1. 시작하기")
 # print("2. 종료하기")
 # print("==============")
-
+#
 # user_input = -1
 #
 # while user_input 1!=2:
 #     user_input = int(input("값을 입력하세요 >>"))
+#
+#
+# while True:
+#     print("1. 시작하기")
+#     print("2. 종료하기")
+#     print("==============")
+#
+#     user_input = int(input("값을 입력하세요 >>"))
+#     if user_input == 2:
+#         break
 
+import random
+
+
+answer = random.randrange(0,10)
+user_input = -1
+
+#사용자가 answer 맞출때까지 반복
 
 while True:
-    print("1. 시작하기")
-    print("2. 종료하기")
-    print("==============")
-
     user_input = int(input("값을 입력하세요 >>"))
-    if user_input == 2:
+
+    if user_input == answer:
+        print("정답입니다!")
         break
+
+
